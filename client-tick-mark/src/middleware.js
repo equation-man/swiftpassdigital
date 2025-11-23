0ebf2eec9e387675479d0f4d80e68c04464da213
equation-man <bigtechguyz@gmail.com>
2025-11-23 10:59:00 +0300

    Middleware fix

--- client-tick-mark/src/middleware.js
+++ client-tick-mark/src/middleware.js
@@ -78,9 +78,11 @@ export default auth(async function middleware(req) {
 export const config = {
   matcher: [
     // Skip Next.js internals and static files
-    '/((?!_next/|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    //'/((?!_next/|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    //'/(api|trpc)(.*)',
+    // Prevents recursion on /api/auth/* in production
+    '/((?!_next/|api/|.*\\..*).*)',
   ],
 };
 
